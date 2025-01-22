d3583dd97c6304effbf5d61833e29ddfdbb0328d
Hassaan200 <hassaankhaliq45@gmail.com>
2025-01-22 14:57:28 +0500

    change by hassan

--- Hassan/contact.js
+++ Hassan/contact.js
@@ -1,5 +1,13 @@
+
 function toggleSearch() {
     const searchInput = document.querySelector('.search-input');
     searchInput.classList.toggle('show');
   }
   
+
+  function values(e){
+    e.preventDefault()
+    console.log("User Name: ",document.getElementById('h-name').value)
+    console.log("User Email: ",document.getElementById('h-email').value)
+    console.log("User Message: ",document.getElementById('h-message').value)
+  }
